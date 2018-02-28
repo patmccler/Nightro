c052672b6099788df819515dd96920212a5b27d4
Patrick McClernan <patmccler@gmail.com>
2018-02-28 16:22:11 -0500

    updated testMain to load on DOMCONTENT loaded

--- Nightro/js/testMain.js
+++ Nightro/js/testMain.js
@@ -1,22 +1,37 @@
 
+function onError(e) {
+  console.log(e);
+}
+
 function wipeAll() {
    console.log("testMain.js");
-  let body = document.querySelector("body");
+  let body = document.getElementsByTagName("body")[0];
   body.style.removeProperty("background");
 
-  let allElem = document.getElementsByTagName("*");
-
+  let allElem = body.getElementsByTagName("*");
+  let count = 0;
   for(let i = 0; i< allElem.length; i++ ) {
 
-    if(allElem[i] instanceof HTMLElement && allElem[i] != body) {
+    if(allElem[i] instanceof HTMLElement) {
       allElem[i].style.removeProperty("background");
       allElem[i].style.background = "inherit";
-      allElem[i].style.color = "white";
+      allElem[i].style.color = "inherit";
+      count++;
     }
 
 
   }
-
+  console.log(count);
 }
+document.addEventListener("DOMContentLoaded", tryWipe);
 
-setTimeout(wipeAll, 1000);//runs on page load
+function tryWipe() {
+  try {
+    wipeAll();
+  }
+  catch (e)
+  {
+    onError(e);
+  }
+}
+//setTimeout(wipeAll, 1000);//runs on page load
