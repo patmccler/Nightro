14031aa1d504e946e14d0e5e95798564fdd4ea5b
Patrick McClernan <patmccler@gmail.com>
2018-03-10 07:46:00 -0500

    Merge branch 'master' into pat-branch
    keeping up with master code

--- Nightro/js/testMain.js
+++ Nightro/js/testMain.js
@@ -7,40 +7,11 @@ function onError(e) {
 document.addEventListener("DOMContentLoaded", setup);
 
 function setup() {
-  tryWipe();
   setupPageAction();
   //TODO Send check request to BG, if need to load, load css
 
 }
 
-function wipeAll() {
-   console.log("testMain.js");
-  let body = document.getElementsByTagName("body")[0];
-  body.style.removeProperty("background");
-
-  let allElem = body.getElementsByTagName("*");
-  let count = 0;
-  for(let i = 0; i< allElem.length; i++ ) {
-
-    if(allElem[i] instanceof HTMLElement) {
-      allElem[i].style.removeProperty("background");
-      allElem[i].style.background = "inherit";
-      allElem[i].style.color = "inherit";
-      count++;
-    }
-  }
-  console.log(count);
-}
-
-function tryWipe() {
-  try {
-    wipeAll();
-  }
-  catch (e)
-  {
-    onError(e);
-  }
-}
 
 function setupPageAction() {
 
