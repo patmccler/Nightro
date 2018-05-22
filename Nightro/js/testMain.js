a52325eb715b8113c525e24291b2a793fc74df5c
patmccler <patmccler@gmail.com>
2018-05-22 13:39:43 -0400

    fixed merge issue

--- Nightro/js/testMain.js
+++ Nightro/js/testMain.js
@@ -30,7 +30,6 @@ function turnOnDarkMode() {
   //TODO
 }
 
-<<<<<<< HEAD
 function currentDomainNeedsDarkMode(darkModeDomain) {
   let currDomain = window.location.hostname;
   console.log(currDomain);
@@ -38,10 +37,6 @@ function currentDomainNeedsDarkMode(darkModeDomain) {
   return darkModeDomain == currDomain;
 }
 
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 5917ca19d3449c22cd0f696146f1eaa9dd84f218
 function wipeAll() {
   console.log("testMain.js");
   let body = document.getElementsByTagName("body")[0];
@@ -67,7 +62,6 @@ function tryWipe() {
     onError(e);
   }
 }
->>>>>>> 1b30493c4318cff99f9669f217d4d854609b9141
 
 function setupPageAction() {
   chrome.runtime.sendMessage({ greeting: "try darkmode page action" }, function(
